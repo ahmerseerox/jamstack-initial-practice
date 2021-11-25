72c3ee65a15f6c83465837fcf53c23f7415513ea
ahmerseerox <ahmer@seerox.com>
2021-11-25 14:22:58 +0500

    added dynamic API to get data

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -2,8 +2,7 @@
   const vibes = [
     "...and you are awesome!",
     "...have a wonderful day!",
-    "...and you've got this!",
-    "...and so is this puppy! 🐶"
+    "...and you've got this!"
   ];
 
    // choose a random good vibe
